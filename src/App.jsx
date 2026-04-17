7b45fcc211e06ffbf61db79e602e78ef585b0a88
JJING_WONG <snow020216@gmail.com>
2026-04-17 14:14:59 +0900

    Test: CollaboCard 컴포넌트 테스트 추가 (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import CollaboCard from "./components/common/CollaboCard";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-500">Tailwind 작동 확인!</h1>
+    <>
+      <h1 className="text-3xl font-bold text-blue-500">Tailwind 작동 확인!</h1>
+      <CollaboCard
+        title="동아리 협업 모집"
+        author="윤현승"
+        time="3시간 전"
+        content="디자인 파트와 협업할 프론트엔드 팀원을 모집합니다."
+        dDay="D-03"
+      />
+    </>
   );
 }
 export default App;
