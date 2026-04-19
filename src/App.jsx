b7f55901a190c0545f4818767bfdaafcbcb29495
JJING_WONG <snow020216@gmail.com>
2026-04-19 23:00:57 +0900

    Refactor: createdAt 문자열을 직접 전달하도록 App.jsx 선행 수정 - elapsedText.js 수정 예정 (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import CollaboCard from "./components/common/CollaboCard";
 import CoffeeChatCard from "./components/common/CoffeeChatCard";
 import FeedCard from "./components/common/FeedCard";
+import { elapsedText } from "./utils/elapsedText";
 
 function App() {
+  const collabo = {
+    collabId: 1,
+    title: "동아리 협업 모집",
+    writerName: "윤현승",
+    createdAt: "2026-04-17T02:00:00", // 확인하고 싶으면 여기서 수정
+    content: "디자인 파트와 협업할 프론트엔드 팀원을 모집합니다.",
+    dDay: "D-03",
+  }
+
   return (
     <>
       <h1 className="text-3xl font-bold text-blue-500">Tailwind 작동 확인!</h1>
       <CollaboCard
-        title="동아리 협업 모집"
-        author="윤현승"
-        time="3시간 전"
-        content="디자인 파트와 협업할 프론트엔드 팀원을 모집합니다."
-        dDay="D-03"
+        id={collabo.collabId}
+        title={collabo.title}
+        author={collabo.writerName}
+        time={elapsedText(collabo.createdAt)}
+        content={collabo.content}
+        dDay={collabo.dDay}
       />
       <CoffeeChatCard
         id={1}
