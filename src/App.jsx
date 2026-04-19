f6599bc6ca888501e02e137e5ccbdc065ee5c2bd
JJING_WONG <snow020216@gmail.com>
2026-04-19 20:04:42 +0900

    Test: FeedCard 컴포넌트 테스트 추가 (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import CollaboCard from "./components/common/CollaboCard";
 import CoffeeChatCard from "./components/common/CoffeeChatCard";
+import FeedCard from "./components/common/FeedCard";
 
 function App() {
   return (
@@ -19,6 +20,17 @@ function App() {
         meetingType="오프라인"
         image="https://placehold.co/206x206"
         onMoreClick={() => alert("잘 눌러지나 테스트")}
+      />
+      <FeedCard
+        id={1}
+        author="윤현승"
+        date="2026년 04월 21일"
+        image="https://placehold.co/600x250"
+        content="대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 
+        대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 
+        대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 
+        대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 대충 아무 내용 적기 "
+        profileImage="https://placehold.co/100x100"
       />
     </>
   );
