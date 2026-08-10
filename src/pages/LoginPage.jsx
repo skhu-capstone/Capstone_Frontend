051574b7530ccad008072d955654939a1f45a80c
JJING_WONG <snow020216@gmail.com>
2026-08-10 23:36:47 +0900

    Refactor: 로그인 속도 개선 #11

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,9 +2,11 @@ import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
+import { useAuth } from "../context/AuthContext";
 
 export default function LoginPage() {
   const navigate = useNavigate();
+  const { setAuthenticatedUser } = useAuth();
 
   const loginMutation = useMutation({
     mutationFn: async (googleAccessToken) => {
@@ -18,12 +20,10 @@ export default function LoginPage() {
     },
 
     onSuccess: (userData) => {
-      localStorage.setItem("accessToken", userData.accessToken);
-      localStorage.setItem("refreshToken", userData.refreshToken);
-      localStorage.setItem("user", JSON.stringify(userData));
+      setAuthenticatedUser(userData);
 
       if (userData.isVerified) {
-        window.location.href = "/";
+        navigate("/");
       } else {
         navigate("/email-verify");
       }
@@ -38,7 +38,6 @@ export default function LoginPage() {
   const googleLogin = useGoogleLogin({
     onSuccess: (tokenResponse) => {
       loginMutation.mutate(tokenResponse.access_token);
-      console.log("로그인 성공");
     },
 
     onError: () => {
